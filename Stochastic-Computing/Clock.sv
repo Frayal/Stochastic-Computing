2a0799db7593999c165144347f7282234110c943
Alexis <alexis.blanchet@telecom-paristech.fr>
2016-06-17 14:15:50 +0200

    forgot one module

--- Stochastic-Computing/Clock.sv
+++ Stochastic-Computing/Clock.sv
@@ -5,9 +5,15 @@
 /**
  * Module: Clock
  * 
- * TODO: Add module documentation
+ * Cloc allowing the system to be synchronous
  */
-module Clock;
+module Clock(output clk);
+	logic clk;
+	always@(*)
+		begin
+			#5 clk = 1;
+			#5 clk = 0;
+		end
 
 
 endmodule
