8108662f29b9836e64471de4316c82c7e22d3cbc
Alexis <alexis.blanchet@telecom-paristech.fr>
2016-06-20 16:52:54 +0200

    last modif

--- Stochastic-Computing/Convert.sv
+++ Stochastic-Computing/Convert.sv
@@ -8,12 +8,70 @@
  * TODO: Add module documentation
  */
 module Convert(		maxnum,
-					SCnum,
 					clk,
-					Bnum);
+					reset,
+					seed
+					Bnum,
+					SCnum);
+
 	
-	
-	
+localparam lenght = 8;	
+
+input [7:0] seed;
+input [7:0] maxnum;
+input clk;
+input [length-1:0] Bnum; 
+
+output SCnum;
+
+logic truc = 0'b0;
+logic  [0:0] nextbit; 
+logic [lenght-1] num;
+logic [lenght-1] lfsr = seed;
+logic [lenght-1] cmpt;
+
+always @(posedge clk )
+begin 
+	if(reset)
+		lfsr <= seed;
+	else
+		if(cmpt==0)
+		begin 
+			num <= (Bnum*(2**lenght-1))/maxnum; // put the right number to compare
+			nextbit <= (lfsr[5]^lfsr[7])^(lfsr[4]^lfsr[3])+0'b1; //xor 
+			lfsr <= lfsr*2 + nextbit ;
+			if (num < lsfr)
+			begin
+				Bnum <= 1'b0;
+				truc <= {truc,Bnum};
+			end
+			else 
+			begin
+				Bnum <= 1'b1;
+				truc <= {truc,Bnum};
+			end	
+			cmpt <= cmpt + 1'b1;
+		end
+		else
+		begin 
+			nextbit <= lfsr[3]^lfsr[7]; //xor 
+			lfsr <= lfsr*2 + nextbit ;
+			if (num < lsfr)
+			begin
+				Bnum <= 1'b0;
+				truc <= {truc,Bnum};
+			end
+			else 
+			begin
+				Bnum <= 1'b1;
+				truc <= {truc,Bnum};
+			end		
+			cmpt <= cmpt + 1'b1;
+		end
+end
+			
+		
+		
 
 
 endmodule
