0e2495653ca9619c20cec408a03b583a654ca148
Antoine DENIS <antoine.denis@telecom-paristech.fr>
2016-06-20 14:08:48 +0200

    Modifications mineures

---
+++ Stochastic-Computing/mux.sv
@@ -0,0 +1,23 @@
+/****************************************************************************
+ * mux.sv
+ ****************************************************************************/
+
+/**
+ * Module: mux
+ * 
+ * TODO: Add module documentation
+ */
+module mux (q, control, a, b);
+	output q;
+	reg q;
+	input control, a, b;
+	
+	wire notcontrol;
+	
+	always@(control or notcontrol or a or b)
+		q=(control & a) | (notcontrol & b);
+	
+	not (notcontrol, control);
+endmodule
+
+
