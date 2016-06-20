0b82e91610e3f284d7b85dfae07f0f0997038aed
Alexis <alexis.blanchet@telecom-paristech.fr>
2016-06-20 10:56:25 +0200

    size correction

--- Stochastic-Computing/Reconverttobinary.sv
+++ Stochastic-Computing/Reconverttobinary.sv
@@ -7,21 +7,31 @@
  * 
  * TODO: Add module documentation
  */
-module Reconverttobinary(	input [8:0] maxnum
-							input [3:0] length,
-							input [(2**length)-1:0] SCnum,
-							input clk,
-							output [length-1:0] Bnum);
-	logic[length-1:0] cmpt;
-	logic[length-1:0] newnum;
+module Reconverttobinary(	maxnum,
+							SCnum,
+							clk,
+							Bnum);
+	localparam length = 4;
+	input [7:0] maxnum,
+	input [(2**length)-1:0] SCnum,
+	input clk,
+	output [length-1:0] Bnum);
+	
+	reg [length-1:0] cmpt;
+	reg [length-1:0] newnum;
+	
+	
 	always@(posedge clk)
-		while(cmpt=>0)
-			newnumb <= newnumb + SCnum[cmpt];
-		
-		Bnum <= ((newnum)*(maxnum))/(length);
-		cmpt <=(2**length)-1;
-		newnum <= 0;
+		begin
+			while(cmpt>0)
+				newnumb <= newnumb + SCnum[cmpt];
 			
+			
+			newnum <= newnnum + SCnumb[0];
+			Bnum <= ((newnum)*(maxnum))/(length);
+			cmpt <=(2**length)-1;
+			newnum <= 0;
+		end	
 
 
 endmodule
